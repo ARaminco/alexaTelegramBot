412e6fe5e4871763b62ee9e6c913a7a72f0696b6
Aliasghar Ramin <ar.p360@hotmail.com>
2017-03-27 03:16:56 +0430

    update

--- bot.php
+++ bot.php
@@ -1,13 +1,10 @@
 <?php
-
 $basedata 		= file_get_contents("php://input");
 $data 		= json_decode(file_get_contents("php://input"), true);
-$botapi = "275868918:AAEyC_Imz8r8nbIOxQD3Kt7mSUF80x7AxUk";
+$botapi = "275868918:AAFQepdGEkgPcvqEVqjG0qJVbbd2BE04Rm8";
 $botid= "0";
 $adminID = "71595348";
 //$shamsidate = jstrftime('%H:%M:%S | %A, %e / %B / %Y');
-
-
 var_dump($data);
 $cid = $data ["message"]["chat"]["id"];
 $uid = $data ["message"]["from"]["id"];
@@ -22,7 +19,6 @@ $fname = $data["message"]["from"]["first_name"];
 $lname = $data["message"]["from"]["last_name"];
 $reply = $data["message"]["reply_to_message"]["text"];
 $callback = $data ["callback_query"]["data"];
-
 if($callback != ""){
 	$cid = $data ["callback_query"]["from"]["id"];
     $uid = $cid;
@@ -30,9 +26,6 @@ if($callback != ""){
 	//$sendM = sendMessage($cid, $text, $botapi ,$encodedMarkup);
 	
 }
-
-
-
 function sendMessage($cid, $text, $botapi ,$encodedMarkup) {
 	$text = urlencode($text);
 	$curl = curl_init("https://api.telegram.org/bot$botapi/sendMessage");
@@ -66,7 +59,6 @@ function sendDocument($cid, $doc, $botapi ,$caption) {
 function chekAlexaRank ($domain){
     $sWebSite=$domain;
     if($source = simplexml_load_file('http://data.alexa.com/data?cli=10&url='.$sWebSite)){
-
         if($source->SD->COUNTRY['RANK']){
             $country    = $source->SD->COUNTRY['RANK'];
             $country_name = $source->SD->COUNTRY['NAME'];
@@ -88,7 +80,6 @@ function chekAlexaRank ($domain){
         $popularity='Not Available';
         $reach='Not Available';
     }
-
     //echo "<div class='text-center'>";
     //echo $country_name .' Rank : '.$country .'<br>';
     //echo ' Alexa Traffic Rank :' . $reach;
@@ -105,15 +96,11 @@ function chekAlexaRank ($domain){
 	📊 رتبه در کشور : $country
 	
 	ربات برسی الکسا : @alexaTelegramBot
+		دانلود رایگان سورس این ربات :
+	https://goo.gl/8YNSTw
 	";
 	return $output ;
 }
-
-
-
-
-
-
 if($inText=="/start"){
 	$text = "
 	به ربات الکسا خوش آمدید !
@@ -124,6 +111,8 @@ if($inText=="/start"){
 	@aliasgharraminch
 	@aliasghar_ramin
 	
+	دانلود رایگان سورس این ربات :
+	https://goo.gl/8YNSTw
 	";
 	$sendM = sendMessage($cid, $text, $botapi ,$encodedMarkup);
 }else{
@@ -134,5 +123,4 @@ if($inText=="/start"){
 	$sendP = sendPhoto($cid, $photo, $botapi ,$caption ,$encodedMarkup) ;
 }
 ?>
-
 
